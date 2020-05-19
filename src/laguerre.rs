612f4f5f36f10070a1f1ebbcaf25038da596a658
Andy Wang <cbeuw.andy@gmail.com>
2020-05-19 14:03:09 +0100

    De-curry

--- src/laguerre.rs
+++ src/laguerre.rs
@@ -11,27 +11,39 @@ fn divide_product<T: Integer + Clone>(r: T, a: T, b: T) -> T {
     (r / g.clone()) / (a * (b / g))
 }
 
-pub fn laguerre_polynomial(n:u64, alpha:u64) -> impl Fn(f64) -> f64 {
-    let facts: Vec<u64> = (0..=n+alpha).scan(1, |prod, x|{
-        *prod = *prod * if x==0 {1} else {x};
-        Some(*prod)
-    }).collect();
+pub struct Laguerre {
+    n:u64,
+    coeffs: Vec<f64>,
+}
 
-    let fact = |i: u64|{facts[i as usize]};
+impl Laguerre{
+    pub fn new(n: u64, alpha: u64) -> Self{
+        let facts: Vec<u64> = (0..=n+alpha).scan(1, |prod, x|{
+            *prod = *prod * if x==0 {1} else {x};
+            Some(*prod)
+        }).collect();
+    
+        let fact = |i: u64|{facts[i as usize]};
+    
+        // TODO: divide_product may be better? 
+        let coeffs: Vec<f64> = (0..=n).rev().map(|j|{
+            (   fact(n+alpha) 
+                / 
+                (fact(j) * fact(alpha+(n-j)))
+            ) as f64
+            /
+            fact(n-j) as f64
+        }).collect();
 
-    // TODO: divide_product may be better? 
-    let coeffs: Vec<f64> = (0..=n).rev().map(|j|{
-        (   fact(n+alpha) 
-            / 
-            (fact(j) * fact(alpha+(n-j)))
-        ) as f64
-        /
-        fact(n-j) as f64
-    }).collect();
+        Laguerre{
+            n,
+            coeffs
+        }
+    }
 
-    move |x:f64| {
-        (0..=n).fold(0.0, |sum: f64, i|{
-            let delta:f64 = coeffs[i as usize] * x.powi(i as i32);
+    pub fn L(&self, x:f64) -> f64{
+        (0..=self.n).fold(0.0, |sum: f64, i|{
+            let delta:f64 = self.coeffs[i as usize] * x.powi(i as i32);
             if i % 2 == 0 {
                 sum + delta
             } else {
--- src/wavefunction.rs
+++ src/wavefunction.rs
@@ -3,41 +3,59 @@ use sphrs::{ComplexSHType, Coordinates, SHEval};
 use num_complex::Complex64;
 use std::f64::consts;
 
-const FRAC_1_REDUCED_BOHR_RADIUS:f64 = 1.888706223265207e10;
-const FRAC_1_REDUCED_BOHR_RADIUS_CUBED:f64 = 6.73741398899e30;
+const FRAC_1_REDUCED_BOHR_RADIUS_ATTO:f64 = 1.888697507170487e-8;
+const FRAC_1_REDUCED_BOHR_RADIUS_CUBED_ATTO:f64 = 6.737320712965956e-24;
+pub const REDUCED_BOHR_RADIUS_ATTO:u64 = 52_946_541;
 
-pub fn wavefuncion(n: u64, l: u64, m:i64) -> impl Fn(f64,f64,f64) -> Complex64{
-    let facts: Vec<_> = (0..=n+l).scan(1, |prod, x|{
-        *prod = *prod * if x==0 {1} else {x};
-        Some(*prod)
-    }).collect();
+pub struct Orbital{
+    n:u64,
+    l:u64,
+    m:i64,
 
-    let root_term = (
-        FRAC_1_REDUCED_BOHR_RADIUS_CUBED
-        *
-        (8 * facts[(n-l-1) as usize]) as f64
-        /
-        (n*n*n*n * 2 * facts[(n+l) as usize]) as f64
-    ).sqrt();
+    laguerre: laguerre::Laguerre,
+    root_term: f64,
+}
 
-    let laguerre_L = laguerre::laguerre_polynomial(n-l-1, 2*l+1);
+impl Orbital{
+    pub fn new(n: u64, l:u64, m:i64) -> Self{
+        let facts: Vec<_> = (0..=n+l).scan(1, |prod, x|{
+            *prod = *prod * if x==0 {1} else {x};
+            Some(*prod)
+        }).collect();
     
-    move |r:f64, theta: f64, phi:f64| {
-        let rho = (r * 2.0 / n as f64) * FRAC_1_REDUCED_BOHR_RADIUS;
-        let unit_sphere: Coordinates<f64> = Coordinates::spherical(1.0, theta, phi);
-        print!("rou: {}\n", rho);
-        print!("laguerre: {}\n",laguerre::laguerre_polynomial(n-l-1, 2*l+1)(rho));
-        print!("harmonics: {}\n", ComplexSHType::Spherical.eval(l as i64,m as i64,&unit_sphere));
+        let root_term = (
+            FRAC_1_REDUCED_BOHR_RADIUS_CUBED_ATTO
+            *
+            (8 * facts[(n-l-1) as usize]) as f64
+            /
+            (n*n*n*n * 2 * facts[(n+l) as usize]) as f64
+        ).sqrt();
+    
+        let laguerre = laguerre::Laguerre::new(n-l-1, 2*l+1);
 
+        Orbital{
+            n,l,m,
+            laguerre,
+            root_term
+        }
+    }
+
+    pub fn psi(&self,r:f64, theta:f64, phi:f64) -> Complex64{
+        let rho = (r * 2.0 / self.n as f64) * FRAC_1_REDUCED_BOHR_RADIUS_ATTO;
+        let unit_sphere: Coordinates<f64> = Coordinates::spherical(1.0, theta, phi);
 
         Complex64::new(
-            root_term                   *
+            self.root_term              *
             consts::E.powf(-rho / 2.0)  *
-            rho.powi(l as i32)          *
-            laguerre_L(rho)
+            rho.powi(self.l as i32)     *
+            self.laguerre.L(rho)
             ,
             0.0) 
-        * 
-        ComplexSHType::Spherical.eval(l as i64,m as i64,&unit_sphere)
+        *
+        ComplexSHType::Spherical.eval(self.l as i64,self.m as i64,&unit_sphere)
+    }
+
+    pub fn probability(&self,r:f64, theta:f64, phi:f64) -> f64 {
+        self.psi(r,theta,phi).norm_sqr() * (r * r) * theta.sin()
     }
 }
