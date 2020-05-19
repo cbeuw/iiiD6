5d4ed60341b96ca36c8e7eea5ccd113a4cac4d96
Andy Wang <cbeuw.andy@gmail.com>
2020-05-19 23:02:53 +0100

    Working code

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use sampler::Sampler;
 use image::{ImageBuffer, Rgb};
 use std::f64::consts::PI;
 
-const IMG_SIZE:usize = 250;
-const ITERS:usize = 10000;
+const IMG_SIZE:usize = 1000;
+const ITERS:usize = 100000;
 fn main() {
     //let orbital = orbital::Orbital::new(2, 1, 1);
     //print!("{:?}\n", orbital.psi(1.0e-10,PI/4.0, PI));
@@ -16,10 +16,10 @@ fn main() {
     let container = vec![0;3*IMG_SIZE*IMG_SIZE];
     let mut image = ImageBuffer::<Rgb<u8>,_>::from_raw(IMG_SIZE as u32, IMG_SIZE as u32, container).unwrap();
 
-    let mut sampler = Sampler::new(1,0,0, IMG_SIZE);
+    let mut sampler = Sampler::new(3,1,0, IMG_SIZE);
     let grid = sampler.sample(ITERS);
 
-    let blue = Rgb([0,0,255]);
+    let blue = Rgb([83,202,236]);
     let black = Rgb([0,0,0]);
 
     for (x, y, pixel) in image.enumerate_pixels_mut() {
--- src/sampler.rs
+++ src/sampler.rs
@@ -4,75 +4,76 @@ use rand::{Rng, SeedableRng};
 use rand::rngs::SmallRng;
 use rayon::prelude::*;
 
-const DEFAULT_R_BOUND:f64 = 4e-10; // 4 angstrom
+const DEFAULT_R_BOUND:f64 = 10e-10; // 4  angstrom
 //const DEFAULT_DELTA_VOLUME:f64 = 1e-30; // 1 cubic angstrom
 const ZERO_PHI_PLANE:f64 = 0.0;
+const PROB_AMPLIFICATION:f64 = 50.0;
 pub struct Sampler{
-    delta_volume: f64,
-    phi_plane: f64,
-
-    orbital: orbital::Orbital,
-    grid: Vec<Vec<bool>>,
-
-    coords: Vec<Vec<(f64,f64)>>
+    grid: Vec<Vec<bool>>,   
+    probs: Vec<Vec<f64>>
 }
 
 impl Sampler{
     pub fn new(n: u64, l:u64, m:i64, grid_size:usize) -> Self{
-        let norm_factor =DEFAULT_R_BOUND as f64/ (grid_size as f64 / 2.0);
+        let mut probs = Vec::new();
+        let delta_volume =  (DEFAULT_R_BOUND / (grid_size as f64 / 2.0 - 1.0)).powi(3);
 
-        let mut coords = Vec::new();
+        let orbital = orbital::Orbital::new(n,l,m);
 
         let grid_isize = grid_size as isize;
-
+        let norm_factor =DEFAULT_R_BOUND as f64/ (grid_size as f64 / 2.0 - 1.0);
         for i in 0..grid_isize{
             let z = (grid_isize/2 - i) as f64 * norm_factor;
-            let mut row_coords = Vec::new();
+            let mut row_probs = Vec::new();
             for j in 0..grid_isize{
                 let x = (j - grid_isize/2) as f64 * norm_factor ;
 
                 let r = (x*x+z*z).sqrt();
-                let theta = (x as f64/z as f64).atan();
-                row_coords.push((r,theta));
+                //let theta = if z != 0.0 {(x/z).atan()} else {0.0};
+                let theta = if (j - grid_isize/2) == 0 || (grid_isize/2 - i) == 0 {0.0} else {(z/r).acos()};
+                let mut prob = orbital.probability(r,theta,ZERO_PHI_PLANE,delta_volume);
+
+                prob *= PROB_AMPLIFICATION;
+
+                if prob < 0.0 || prob > 1.0 {
+                    panic!(format!("bad prob {}", prob));
+                }
+
+                row_probs.push(prob);
             }
-            coords.push(row_coords);
+            //println!("({}): {}",i,row_probs.iter().cloned().fold(0./0., f64::max));
+            probs.push(row_probs);
         }
 
         Sampler{
-            delta_volume: (DEFAULT_R_BOUND / (grid_size as f64 / 2.0)).powi(3),
-            phi_plane: ZERO_PHI_PLANE,
-
-            orbital: orbital::Orbital::new(n,l,m),
             grid: vec![vec![false;grid_size];grid_size],
 
-            coords
+            probs
         }
     }
 
     pub fn sample(&mut self, iter_per_row: usize) -> &Vec<Vec<bool>>{
-        let orbital = &self.orbital;
-        let coords = &self.coords;
-        let phi = self.phi_plane;
-        //let volume = DEFAULT_DELTA_VOLUME;
-        let volume = self.delta_volume;
-        //let mut val = String::new();
+        let probs = &self.probs;
         self.grid.par_iter_mut().enumerate().for_each(|(i,row)|{
             let mut rng = SmallRng::from_entropy();
-            //let mut largest = 0.0;
             for _ in 0..iter_per_row {
                 for (j, hit) in row.iter_mut().enumerate() {
-                    let (r,theta) = coords[i][j];
-                    let prob = orbital.probability(r,theta,phi,volume);
-                    //val = format!("{} {1:+e}",val, prob);
-                    //if prob > largest { largest = prob};
-                    //if prob < smallest{smallest = prob};
-                    *hit |= rng.gen_bool(prob);
+                    *hit |= rng.gen_bool(probs[i][j]);
                 };
             }
-            //println!("{}",val);
         });
-        //println!("{}", largest);
-        //println!("{}", smallest);
+        &self.grid
+    }
+
+    pub fn sample_mono(&mut self, iter_per_row: usize) -> &Vec<Vec<bool>>{
+        let probs = &self.probs;
+        self.grid.par_iter_mut().enumerate().for_each(|(i,row)|{
+            let mut rng = SmallRng::from_entropy();
+                for (j, hit) in row.iter_mut().enumerate() {
+                    let prob = 1.0-(1.0-probs[i][j]).powi(iter_per_row as i32);
+                    *hit |= rng.gen_bool(prob);
+                };
+        });
         &self.grid
     }
 }
