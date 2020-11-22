6924cbe5c8d7011c70f68691131245a914cf1743
Andy Wang <cbeuw.andy@gmail.com>
2020-11-22 22:34:14 +0000

    Pass in the coordinates directly without manual conversion to spherical system

--- src/orbital.rs
+++ src/orbital.rs
@@ -58,16 +58,11 @@ impl Orbital {
         self.n
     }
 
-    fn cplx_sph_harmonics(&self, unit_sphere: &Coordinates<f64>) -> Complex64 {
-        ComplexSHType::Spherical.eval(self.l as i64, self.m, unit_sphere)
+    fn cplx_sph_harmonics(&self, coord: &Coordinates<f64>) -> Complex64 {
+        ComplexSHType::Spherical.eval(self.l as i64, self.m, coord)
     }
 
-    fn phase(
-        &self,
-        sph_harmonics: Complex64,
-        radial: f64,
-        unit_sphere: &Coordinates<f64>,
-    ) -> Phase {
+    fn phase(&self, sph_harmonics: Complex64, coord: &Coordinates<f64>) -> Phase {
         // Phase calculation
         // This is the sign of R(r)Y_(m, l)(theta, phi), but Y_(m, l) is in its real form
         // (since we can't take the sign) of a the complex number psi
@@ -76,7 +71,7 @@ impl Orbital {
         let condon_shortley_sign = if self.m.abs() % 2 == 0 { 1. } else { -1. };
         let r_sph_harm = if self.m < 0 {
             // Since we need to calculate Y_(-m,l) anyway, it's cheaper to calculate the real value directly
-            condon_shortley_sign * RealSHType::Spherical.eval(self.l as i64, self.m, unit_sphere)
+            condon_shortley_sign * RealSHType::Spherical.eval(self.l as i64, self.m, coord)
         } else if self.m == 0 {
             sph_harmonics.re
         } else {
@@ -94,14 +89,14 @@ impl Orbital {
 
     #[inline(always)]
     fn psi_with_phase(&self, coord: Coordinates<f64>) -> (Complex64, Phase) {
-        let unit_sphere: Coordinates<f64> = Coordinates::spherical(1.0, coord.theta(), coord.phi());
+        // let unit_sphere: Coordinates<f64> = Coordinates::spherical(1.0, coord.theta(), coord.phi());
         let rho = coord.r() * self.rho_over_r;
         let radial =
             self.root_term * (-rho / 2.0).exp() * rho.powi(self.l as i32) * self.laguerre.L(rho);
-        let sph_harmonics = self.cplx_sph_harmonics(&unit_sphere);
+        let sph_harmonics = self.cplx_sph_harmonics(&coord);
         let psi = radial * sph_harmonics;
 
-        let phase = self.phase(sph_harmonics, radial, &unit_sphere);
+        let phase = self.phase(sph_harmonics, &coord);
 
         (psi, phase)
     }
@@ -110,11 +105,10 @@ impl Orbital {
     fn psi(&self, coord: Coordinates<f64>) -> Complex64 {
         // psi(r, theta, phi) = R(r)Y_(m, l)(theta, phi)
         // where R(r) is the real radial component, and Y_(m, l)(theta, phi) is the complex spherical harmonic
-        let unit_sphere: Coordinates<f64> = Coordinates::spherical(1.0, coord.theta(), coord.phi());
         let rho = coord.r() * self.rho_over_r;
         let radial =
             self.root_term * (-rho / 2.0).exp() * rho.powi(self.l as i32) * self.laguerre.L(rho);
-        let psi = radial * self.cplx_sph_harmonics(&unit_sphere);
+        let psi = radial * self.cplx_sph_harmonics(&coord);
 
         psi
     }
