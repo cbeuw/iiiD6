9b0eff3ad297b161efce387c079c1680b4ea1112
Andy Wang <cbeuw.andy@gmail.com>
2020-11-23 11:12:51 +0000

    Use a lookup table for factorials

--- src/laguerre.rs
+++ src/laguerre.rs
@@ -3,22 +3,41 @@ pub struct Laguerre {
     coeffs: Vec<f64>,
 }
 
+// all factorials representible in u64, starting from 0!
+const FACTS: [u64; 21] = [
+    1,
+    1,
+    2,
+    6,
+    24,
+    120,
+    720,
+    5040,
+    40320,
+    362880,
+    3628800,
+    39916800,
+    479001600,
+    6227020800,
+    87178291200,
+    1307674368000,
+    20922789888000,
+    355687428096000,
+    6402373705728000,
+    121645100408832000,
+    2432902008176640000,
+];
+
+#[inline(always)]
+fn fact(i: u64) -> u64 {
+    FACTS[i as usize]
+}
 // Generalised Laguerre polynomial
 // https://en.wikipedia.org/wiki/Laguerre_polynomials#Generalized_Laguerre_polynomials
 //
 // Calculated using the closed summation form
 impl Laguerre {
     pub fn new(n: u64, alpha: u64) -> Self {
-        // precompute all factorials up to n+alpha (which is the largest factorial we need)
-        let facts: Vec<u64> = (0..=n + alpha)
-            .scan(1, |prod, x| {
-                *prod = *prod * if x == 0 { 1 } else { x };
-                Some(*prod)
-            })
-            .collect();
-
-        let fact = |i: u64| facts[i as usize];
-
         // precompute the coefficient terms ((n + alpha) Choose (n - i)) / i!
         // TODO: divide_product may be better? But this is not a hot spot
         let coeffs: Vec<f64> = (0..=n)
